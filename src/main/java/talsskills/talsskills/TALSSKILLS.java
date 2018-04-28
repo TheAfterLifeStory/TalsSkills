8e5ff2f2702f85da321f75696ddf867e2e4f7f16
IbuIbu2057 <36985757+IbuIbu2057@users.noreply.github.com>
2018-04-29 00:01:36 +0900

    tals commits

---
+++ src/main/java/talsskills/talsskills/TALSSKILLS.java
@@ -0,0 +1,25 @@
+package talsskills.talsskills;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TALSSKILLS extends JavaPlugin {
+
+    /***************************************************
+     *                                                 *
+     *   Copyright                                     *
+     *           The After Life Story                  *
+     *                              Rights Reserved    *
+     *                                                 *
+     ***************************************************/
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
